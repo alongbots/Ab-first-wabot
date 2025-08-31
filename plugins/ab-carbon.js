7c0cdb558dc0785a8b7d785cbda50d9868132560
ABRAHAM DWAMENA <120536940+abrahamdw882@users.noreply.github.com>
2025-08-31 16:21:33 +0000

    Update ab-carbon.js

--- plugins/ab-carbon.js
+++ plugins/ab-carbon.js
@@ -7,24 +7,26 @@ module.exports = {
     
     async execute(sock, m, args) {
         if (!args || args.length === 0) {
-            return await m.send("❌ Please provide some text to generate Carbon.");
+            return await m.send("Please provide some text to generate Carbon.");
         }
 
         const text = args.join(" ");
         const theme = "seti";
 
         try {
+            await m.react('⌛'); 
+
             const res = await axios.get(`https://ab-carbon.abrahamdw882.workers.dev/?q=${encodeURIComponent(text)}&theme=${theme}`, {
                 responseType: 'arraybuffer'
             });
             const buffer = Buffer.from(res.data, 'binary');
             await sock.sendMessage(m.from, {
                 image: buffer,
-                caption: `Here's your stylized Carbon snippet:`
+                caption: `like what you see?`
             }, { quoted: m._raw });
 
         } catch (err) {
-            await m.send(`❌ Error generating Carbon image:\n\`\`\`\n${err.message}\n\`\`\``);
+            await m.send(` Error generating Carbon image:\n\`\`\`\n${err.message}\n\`\`\``);
         }
     }
 };
