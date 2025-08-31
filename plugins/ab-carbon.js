b19e187140b91c1a8b31a474848533bf32e75448
ABRAHAM DWAMENA <120536940+abrahamdw882@users.noreply.github.com>
2025-08-31 14:58:04 +0000

    Update ab-carbon.js

--- plugins/ab-carbon.js
+++ plugins/ab-carbon.js
@@ -12,23 +12,23 @@ module.exports = {
         '';
     }
 
-    if (!text) {
-      return await m.reply('Please provide text or reply to a message to generate a snippet.');
-    }
+    if (!text) return await m.reply('Please provide text or reply to a message.');
 
     const theme = 'seti';
 
     try {
-      const response = await axios.get(`https://ab-carbon.abrahamdw882.workers.dev/?q=${encodeURIComponent(text)}&theme=${theme}`);
-      const carbonImage = response.data;
+      const response = await axios.get(`https://ab-carbon.abrahamdw882.workers.dev/?q=${encodeURIComponent(text)}&theme=${theme}`, {
+        responseType: 'arraybuffer'
+      });
+      const buffer = Buffer.from(response.data, 'binary');
       await sock.sendMessage(m.from, {
-        image: { url: carbonImage },
-        caption: 'Here\'s your stylized code snippet:'
+        image: buffer,
+        caption: "Here's your stylized code snippet:"
       }, { quoted: m });
-      
-    } catch (error) {
-      console.error('Error generating carbon image:', error);
+
+    } catch (err) {
+      console.error('Error generating carbon image:', err);
       await m.reply('Failed to generate the stylized code snippet. Please try again later.');
     }
-  },
+  }
 };
