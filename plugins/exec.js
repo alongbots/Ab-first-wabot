a2ff3a1996218dbf681a20412a3bae1fe8dac6ec
ABRAHAM DWAMENA <120536940+abrahamdw882@users.noreply.github.com>
2025-09-01 22:30:17 +0000

    Update exec.js

--- plugins/exec.js
+++ plugins/exec.js
@@ -28,11 +28,11 @@ module.exports = {
 
     const code = m.text.slice(1).trim();
 
-    const info = '*ABZTech Exec Result*';
+    const info = '*ABZTech Exec*';
     const imgUrl = 'https://i.ibb.co/KpcF9Gnf/4f41074aab5a035fcac5e111911b2456-1.jpg';
     const author = 'ABZTech';
-    const botname = 'ABZTech Bot';
-    const sourceUrl = 'https://ab-tech-api.vercel.app/';
+    const botname = 'ABZTech ᴍᴜʟᴛɪᴅᴇᴠɪᴄᴇ';
+    const sourceUrl = 'https://abztech.xyz/';
 
     try {
       const sandbox = {
