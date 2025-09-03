aa8aba787c4662e734436d012018b8a0d5d927a6
ABRAHAM DWAMENA <120536940+abrahamdw882@users.noreply.github.com>
2025-09-03 10:49:11 +0000

    Create mention.js

---
+++ plugins/mention.js
@@ -0,0 +1,68 @@
+module.exports = {
+    name: 'mention-owner',
+    description: 'Auto reply + react when owner is tagged in a group',
+
+    async execute() {},
+
+    async onMessage(sock, m) {
+        try {
+            const text = m.body || m.text || m.message?.extendedTextMessage?.text || '';
+           // console.log('New message:', text); YA FOR DEBUGS
+
+            const owners = ['25770239992037', '233533763772'];
+            const isOwnerTagged = owners.some(owner => text.includes(`@${owner}`));
+            if (!isOwnerTagged) return;
+
+            //console.log('Owner was tagged!'); YA FOR DEBUGS
+
+            const name = m.pushName || m.sender.split('@')[0];
+            const audioUrl = 'https://files.catbox.moe/6sbydx.mp3';
+            const thumbnail = 'https://files.catbox.moe/ug3qys.jpg';
+            const quoted = {
+                key: {
+                    fromMe: false,
+                    participant: m.sender,
+                    ...(m.isGroup ? { remoteJid: m.from } : {}),
+                },
+                message: {
+                    contactMessage: {
+                        displayName: name,
+                        vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;a,;;;\nFN:${name}\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`,
+                    },
+                },
+            };
+
+            await m.send(
+                {
+                    audio: { url: audioUrl },
+                    mimetype: 'audio/mpeg',
+                    ptt: true,
+                    waveform: [100, 0, 100, 0, 100, 0, 100],
+                    fileName: 'OwnerTag',
+                    contextInfo: {
+                        mentionedJid: [m.sender],
+                        externalAdReply: {
+                            title: "You tagged my owner Abraham",
+                            body: 'ABZTech ᴍᴜʟᴛɪᴅᴇᴠɪᴄᴇ',
+                            thumbnailUrl: thumbnail,
+                            sourceUrl: 'https://www.whatsapp.com/channel/0029VaMGgVL3WHTNkhzHik3c',
+                            mediaType: 1,
+                            renderLargerThumbnail: true,
+                        },
+                    },
+                },
+                { quoted }
+            );
+
+            await m.send({
+                react: {
+                    text: '✨',
+                    key: quoted.key
+                }
+            });
+
+        } catch (err) {
+            console.error(' Mention-owner plugin error:', err);
+        }
+    }
+};
